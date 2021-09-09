d6487b64d9b6407508efa38a029fef63772df240
Dmitry K <69163014+webdev163@users.noreply.github.com>
2021-09-10 00:42:59 +0400

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!');
+  let result = arr.slice(0);
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] === '--discard-prev' && result[i - 1] !== undefined) result.splice(i - 1, 2);
+    if (result[i] === '--double-prev' && result[i - 1] !== undefined) result[i] = result [i - 1];
+    if (result[i] === '--double-next' && result[i + 1] !== undefined) result[i] = result[i + 1];
+    if (result[i] === '--discard-next' && result[i + 1] !== undefined) result.splice(i, 2);
+  }
+  return result.filter(el => el !== '--discard-prev' && el !== '--double-prev' && el !== '--double-next' && el !== '--discard-next');
 }
