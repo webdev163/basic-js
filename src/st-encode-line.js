e81534f5bcec8a567c82099f4708c05e774536be
Dmitry K <69163014+webdev163@users.noreply.github.com>
2021-09-11 07:57:12 +0400

    feat: add st-encode-line solution

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let counter = 0;
+  let result = '';
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] !== arr[i]) {
+      counter++;
+      counter > 1 ? result += `${counter}${arr[i]}` : result += `${arr[i]}`;
+      counter = 0;
+    } else {
+      counter++;
+    }
+  }
+  return result;
 }
+
+// AssertionError: expected 'aa4att4t' to equal '4a4t'
