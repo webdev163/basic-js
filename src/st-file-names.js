66ce113312bb0c68b3b3507423b588905da171d3
Dmitry K <69163014+webdev163@users.noreply.github.com>
2021-09-13 00:34:15 +0400

    feat: add st-file-names solution

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  let countersObj = {};
+  for (let i = 0; i < names.length; i++) {
+    if (names.slice(0, i).includes(names[i])) {
+      countersObj[names[i]] = (countersObj[names[i]] + 1) || 1;
+      names[i] = `${names[i]}(${countersObj[names[i]]})`;
+    }
+  }
+  return names;
 }
