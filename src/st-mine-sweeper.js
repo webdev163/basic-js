3dc5e04e6522150bdaad5bae841984e72ae7ab3a
Dmitry K <69163014+webdev163@users.noreply.github.com>
2021-09-13 03:15:14 +0400

    feat: add st-mine-sweeper solution

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,46 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === true) {
+        if (i - 1 >= 0 && matrix[i - 1][j] !== undefined && matrix[i - 1][j] !== true) {
+          matrix[i - 1][j]++;
+        }
+        if (i - 1 >= 0 && j + 1 < matrix[i].length && matrix[i - 1][j + 1] !== undefined && matrix[i - 1][j + 1] !== true) {
+          matrix[i - 1][j + 1]++;
+        }
+        if (j + 1 < matrix[i].length && matrix[i][j + 1] !== undefined && matrix[i][j + 1] !== true) {
+          matrix[i][j + 1]++;
+        }
+        if (i + 1 < matrix.length && j + 1 < matrix[i].length && matrix[i + 1][j + 1] !== undefined && matrix[i + 1][j + 1] !== true) {
+          matrix[i + 1][j + 1]++;
+        }
+        if (i + 1 < matrix.length && matrix[i + 1][j] !== undefined && matrix[i + 1][j] !== true) {
+          matrix[i + 1][j]++;
+        }
+        if (i + 1 < matrix.length && j - 1 >= 0 && matrix[i + 1][j - 1] !== undefined && matrix[i + 1][j - 1] !== true) {
+          matrix[i + 1][j - 1]++;
+        }
+        if (j - 1 >= 0 && matrix[i][j - 1] !== undefined && matrix[i][j - 1] !== true) {
+          matrix[i][j - 1]++;
+        }
+        if (i - 1 >= 0 && j - 1 >= 0 && matrix[i - 1][j - 1] !== undefined && matrix[i - 1][j - 1] !== true) {
+          matrix[i - 1][j - 1]++;
+        }
+      }
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === true) {
+        matrix[i][j] = 1;
+      }
+      if (matrix[i][j] === false) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  return matrix;
 }
