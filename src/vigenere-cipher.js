2fa7ad4814a2838d2981525290a11230b1f7a7f9
Dmitry K <69163014+webdev163@users.noreply.github.com>
2021-09-13 16:18:23 +0400

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,67 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    bool === true ? this.reversed = false : this.reversed = true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(str, key) {
+    if (!str || !key) throw new Error('Incorrect arguments!');
+    let counter = 0;
+    const strArr = str.split('').filter(el => /[a-zA-Z]/.test(el));
+    while (strArr.length > key.length) key += key;
+    key = key.slice(0, strArr.length);
+    const keyArr = key.split('');
+    for (let i = 0; i < strArr.length; i++) {
+      if (strArr[i] === strArr[i].toLowerCase() && strArr[i].charCodeAt(0) > 96 && strArr[i].charCodeAt(0) < 123) {
+        const offset = keyArr[i].toLowerCase().charCodeAt(0) - 97;
+        let ndx = strArr[i].charCodeAt(0) + offset;
+        if (ndx > 122) ndx = ndx - 26;
+        strArr[i] = String.fromCharCode(ndx);
+      } else if (strArr[i].charCodeAt(0) > 64 && strArr[i].charCodeAt(0) < 91) {
+        const offset = keyArr[i].toUpperCase().charCodeAt(0) - 65;
+        let ndx = strArr[i].charCodeAt(0) + offset;
+        if (ndx > 90) ndx = ndx - 26;
+        strArr[i] = String.fromCharCode(ndx);
+      }
+    }
+    const result = str.split('').map(el => {
+      if (/[a-zA-Z]/.test(el)) {
+        el = strArr[counter];
+        counter++;
+      }
+      return el;
+    })
+    return this.reversed === true ? result.reverse().join('').toUpperCase() : result.join('').toUpperCase();
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error('Incorrect arguments!');
+    let counter = 0;
+    const strArr = str.split('').filter(el => /[a-zA-Z]/.test(el));
+    while (strArr.length > key.length) key += key;
+    key = key.slice(0, strArr.length);
+    const keyArr = key.split('');
+    for (let i = 0; i < strArr.length; i++) {
+      if (strArr[i] === strArr[i].toLowerCase() && strArr[i].charCodeAt(0) > 96 && strArr[i].charCodeAt(0) < 123) {
+        const offset = keyArr[i].toLowerCase().charCodeAt(0) - 97;
+        let ndx = strArr[i].charCodeAt(0) - offset;
+        if (ndx < 97) ndx = ndx + 26;
+        strArr[i] = String.fromCharCode(ndx);
+      } else if (strArr[i].charCodeAt(0) > 64 && strArr[i].charCodeAt(0) < 91) {
+        const offset = keyArr[i].toUpperCase().charCodeAt(0) - 65;
+        let ndx = strArr[i].charCodeAt(0) - offset;
+        if (ndx < 65) ndx = ndx + 26;
+        strArr[i] = String.fromCharCode(ndx);
+      }
+    }
+    const result = str.split('').map(el => {
+      if (/[a-zA-Z]/.test(el)) {
+        el = strArr[counter];
+        counter++;
+      }
+      return el;
+    })
+    return this.reversed === true ? result.reverse().join('').toUpperCase() : result.join('').toUpperCase();
   }
 }
